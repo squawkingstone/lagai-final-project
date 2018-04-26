9410f3fce0efad12c42037044d76c38d7937ad9b
anniepa <anniepa246@gmail.com>
2018-04-26 17:29:51 -0400

    GoMove implemented
    sadfsadlfkroerienaevlasdknc;alr

--- src/board/GoMove.java
+++ src/board/GoMove.java
@@ -3,10 +3,32 @@ package board;
 import kgs_mcts.Move;
 
 public class GoMove implements kgs_mcts.Move {
+    private int x, y;
+
+    public GoMove(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
 
     @Override
     public int compareTo(Move o) {
-        return 0;
+        GoMove g = (GoMove) o;
+        if(this.x == g.getX()){
+            return this.x - g.getX();
+        }
+        return this.y - g.getY();
     }
 
+    @Override
+    public String toString(){
+        return "place_move " + this.x + " " + this.y;
+    }
 }
